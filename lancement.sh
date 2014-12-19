d7911eeb4a8c2f53ce01fc6baa9ba67200f67bbc
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@c4a22f20-e457-1c9d-744f-fd06b3cebae0>
2014-12-19 15:16:32 +0000

    corrections script + rajout lancement.sh

--- lancement.sh
+++ lancement.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i = 10000; i <= 100000; i=i*10)); do
+for ((i = 10000; i <= 10000000; i=i*10)); do
 	./vectors_generator $i
 	for ((j = 0; j < 10; j++)); do
 		echo "Lancement de variation_nb_thread avec 63 threads, taille des vecteurs de $i et temps dans la section critique de 0.00$j sec"
